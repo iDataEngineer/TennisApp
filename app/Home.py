c16e7117428edd5b28ec4a37cd52ffca62402e3f
Daniel <76977155+iDataEngineer@users.noreply.github.com>
2022-06-25 18:26:50 +0100

    Updated app pages

--- app/Home.py
+++ app/Home.py
@@ -1,37 +1,31 @@
 # Tennis App
-import streamlit as st
+import streamlit as st, PIL, requests
+from io import BytesIO
 from streamlit.logger import get_logger
 
 LOGGER = get_logger(__name__.title())
 
 def run():
-    st.set_page_config(
-        page_title="Home",
-        page_icon="🏛",
-    )
+    st.set_page_config(layout="wide")
 
+    # Homepage items
+    st.title('Welome to the Tennis App...')
+    st.markdown('''---''')
 
-    page_names_to_funcs = {
-        "Home": 'app',
-        "ATP Tour Stats": 'ATP_tour',
-        "Grand Slam Predictions": 'Slam_ML',
-    }
+    st.image(get_header())
 
-    selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
-
-
-    st.title('Tennis App')
-    st.markdown('---')
-
-    st.sidebar.success("Select a demo above.")
-
-    st.markdown(
-        """
-            This is the text bit...    
-        """
-    )
-
+@st.cache(show_spinner=False)
+def get_header(url = 'https://raw.githubusercontent.com/iDataEngineer/ATP-SlamApp/main/data/SlamApp_BG.jpg', new_width = 1200):
+    # get from web
+    req = requests.get(url).content
+    image_req = BytesIO(req)    
+    
+    # format image
+    img = PIL.Image.open(image_req)
+    wpercent = (new_width/float(img.size[0]))
+    hsize = int((float(img.size[1])*float(wpercent)))
+    
+    return img.resize((new_width,hsize), PIL.Image.ANTIALIAS)
 
 if __name__ == "__main__":
     run()
-
--- app/pages/Slam_ML.py
+++ app/pages/Slam_ML.py
@@ -7,126 +7,130 @@ from utils.Processor_SlamML import data_processor, create_merged_data
 ################ APP Set-Up #################
 #############################################
 
-# Wide Mode 
-def auto_wide_mode():
-    st.set_page_config(layout="wide")
+def GrandSlam():
 
-auto_wide_mode()
+    # Wide Mode 
+    def auto_wide_mode():
+        st.set_page_config(layout="wide")
 
-# Dataset
-@st.cache(show_spinner=False)
-def update_data():
-    return data_processor()
+    auto_wide_mode()
 
-data = update_data()
+    # Dataset
+    @st.cache(show_spinner=False)
+    def update_data():
+        return data_processor()
 
-# Header image
-@st.cache(show_spinner=False)
-def get_header(url = 'https://raw.githubusercontent.com/iDataEngineer/ATP-SlamApp/main/data/SlamApp_BG.jpg', new_width = 1200):
-    # get from web
-    req = requests.get(url).content
-    image_req = BytesIO(req)    
-    
-    # format image
-    img = PIL.Image.open(image_req)
-    wpercent = (new_width/float(img.size[0]))
-    hsize = int((float(img.size[1])*float(wpercent)))
-    
-    return img.resize((new_width,hsize), PIL.Image.ANTIALIAS)
+    data = update_data()
 
-#############################################
-################ App Sidebar ################
-#############################################
+    # Header image
+    @st.cache(show_spinner=False)
+    def get_header(url = 'https://raw.githubusercontent.com/iDataEngineer/ATP-SlamApp/main/data/SlamApp_BG.jpg', new_width = 1200):
+        # get from web
+        req = requests.get(url).content
+        image_req = BytesIO(req)    
+        
+        # format image
+        img = PIL.Image.open(image_req)
+        wpercent = (new_width/float(img.size[0]))
+        hsize = int((float(img.size[1])*float(wpercent)))
+        
+        return img.resize((new_width,hsize), PIL.Image.ANTIALIAS)
 
-st.sidebar.header('🎮 App Filters 🎮')
+    #############################################
+    ################ App Sidebar ################
+    #############################################
 
-# players
-players = pd.concat([data['Player 1'], data['Player 2'] ], axis = 0).sort_values().unique()
-selected_player = st.sidebar.multiselect(label='Select Players', options=players)
+    st.sidebar.header('🎮 App Filters 🎮')
 
-# slams
-slams = data['Grand Slam'].unique()
-selected_slam = st.sidebar.multiselect(label='Select Slams', options=slams)
+    # players
+    players = pd.concat([data['Player 1'], data['Player 2'] ], axis = 0).sort_values().unique()
+    selected_player = st.sidebar.multiselect(label='Select Players', options=players)
 
-# years
-years = data['Year'].unique()
-selected_year = st.sidebar.multiselect(label='Select Years', options=years)
+    # slams
+    slams = data['Grand Slam'].unique()
+    selected_slam = st.sidebar.multiselect(label='Select Slams', options=slams)
 
-# round
-rounds = data['Round'].unique()
-selected_round = st.sidebar.multiselect(label='Select Round', options=rounds)
+    # years
+    years = data['Year'].unique()
+    selected_year = st.sidebar.multiselect(label='Select Years', options=years)
 
-#######################
-#### Apply Filters ####
-#######################
+    # round
+    rounds = data['Round'].unique()
+    selected_round = st.sidebar.multiselect(label='Select Round', options=rounds)
 
-# players
-if len(selected_player) != 0:
-    data = data[(data['Player 1'].isin(selected_player)) | (data['Player 2'].isin(selected_player))]
+    #######################
+    #### Apply Filters ####
+    #######################
 
-# slams
-if len(selected_slam) > 0:
-    data = data[data['Grand Slam'].isin(selected_slam)]
+    # players
+    if len(selected_player) != 0:
+        data = data[(data['Player 1'].isin(selected_player)) | (data['Player 2'].isin(selected_player))]
 
-# years
-if len(selected_year) > 0:
-    data = data[data['Year'].isin(selected_year)]
-else:
-    data = data.drop(columns=['Year'])
+    # slams
+    if len(selected_slam) > 0:
+        data = data[data['Grand Slam'].isin(selected_slam)]
 
-# rounds
-if len(selected_round) > 0:
-    data = data[data['Round'].isin(selected_round)]
+    # years
+    if len(selected_year) > 0:
+        data = data[data['Year'].isin(selected_year)]
+    else:
+        data = data.drop(columns=['Year'])
 
-
-############################################
-################ APP Build #################
-############################################
-st.title('🏆 SlamApp: ATP Tour 2022 🏆')
-
-st.markdown ('''
-    This app is used for sharing the output from my SlamModel project...
-    \n
-    The plan is to update as each tournament progresses but also to collect all the results across the year...
-    \n
-    ''')
-
-# Header Image & Source
-st.image(get_header())
-st.markdown('Image: [Josh Calabrese](https://images.unsplash.com/photo-1482614312710-79c1d29bda2a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80)')
+    # rounds
+    if len(selected_round) > 0:
+        data = data[data['Round'].isin(selected_round)]
 
 
-# Model Accuracy Chart
-st.subheader('Model Performance...')
-accuracy_round = data.groupby(by=['Round']).mean()['Predicted']*100
-accuracy_overall = round(accuracy_round.sum() / 7, 1)
+    ############################################
+    ################ APP Build #################
+    ############################################
+    st.title('🏆 SlamApp: ATP Tour 2022 🏆')
 
-st.markdown(f'### Mean model accuracy: {accuracy_overall} %')
-st.bar_chart(accuracy_round)
+    st.markdown ('''
+        This app is used for sharing the output from my SlamModel project...
+        \n
+        The plan is to update as each tournament progresses but also to collect all the results across the year...
+        \n
+        ''')
 
-# Player Results
-st.subheader('Player Results...')
-data_merge = create_merged_data(dataset=data)
-df_players = pd.DataFrame(index = data_merge.mean().index)
-
-df_players['Matches Played'] = df_players.index.map(data_merge.count()['Predicted'].to_dict())
-df_players['Predicted'] = df_players.index.map(data_merge.sum()['Predicted'].to_dict()).astype(int)
-
-df_players['% Accuracy'] = 100 * df_players['Predicted'] / df_players['Matches Played']
-df_players['% Accuracy'] = [int(i) for i in df_players['% Accuracy']]
-
-df_players['Mean Model Win %'] = df_players.index.map(data_merge.mean()['P1 Win %'].to_dict()).astype(int)
-df_players['Mean Model Lose %'] = df_players.index.map(data_merge.mean()['P2 Win %'].to_dict()).astype(int)
-
-df_players['Last Round Played'] = df_players.index.map(data_merge.max()['Round'].to_dict())
-
-# filter 
-if len(selected_player) > 0:
-    df_players = df_players[df_players.index.isin(selected_player)]
-
-st.dataframe(df_players.sort_values(by = ['Predicted', '% Accuracy'], ascending=False))
+    # Header Image & Source
+    st.image(get_header())
+    st.markdown('Image: [Josh Calabrese](https://images.unsplash.com/photo-1482614312710-79c1d29bda2a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80)')
 
 
-# Filtered Data
-st.subheader('Filtered Results...')
-st.dataframe(data)
+    # Model Accuracy Chart
+    st.subheader('Model Performance...')
+    accuracy_round = data.groupby(by=['Round']).mean()['Predicted']*100
+    accuracy_overall = round(accuracy_round.sum() / 7, 1)
+
+    st.markdown(f'### Mean model accuracy: {accuracy_overall} %')
+    st.bar_chart(accuracy_round)
+
+    # Player Results
+    st.subheader('Player Results...')
+    data_merge = create_merged_data(dataset=data)
+    df_players = pd.DataFrame(index = data_merge.mean().index)
+
+    df_players['Matches Played'] = df_players.index.map(data_merge.count()['Predicted'].to_dict())
+    df_players['Predicted'] = df_players.index.map(data_merge.sum()['Predicted'].to_dict()).astype(int)
+
+    df_players['% Accuracy'] = 100 * df_players['Predicted'] / df_players['Matches Played']
+    df_players['% Accuracy'] = [int(i) for i in df_players['% Accuracy']]
+
+    df_players['Mean Model Win %'] = df_players.index.map(data_merge.mean()['P1 Win %'].to_dict()).astype(int)
+    df_players['Mean Model Lose %'] = df_players.index.map(data_merge.mean()['P2 Win %'].to_dict()).astype(int)
+
+    df_players['Last Round Played'] = df_players.index.map(data_merge.max()['Round'].to_dict())
+
+    # filter 
+    if len(selected_player) > 0:
+        df_players = df_players[df_players.index.isin(selected_player)]
+
+    st.dataframe(df_players.sort_values(by = ['Predicted', '% Accuracy'], ascending=False))
+
+    # Filtered Data
+    st.subheader('Filtered Results...')
+    st.dataframe(data)
+
+if __name__ == '__main__':
+    GrandSlam()
