5631c6c719765e2d3000a9859f636fef84c54370
Daniel <76977155+iDataEngineer@users.noreply.github.com>
2022-07-06 17:32:33 +0100

    Diabled slam app

--- app/app.py
+++ app/app.py
@@ -2,23 +2,24 @@
 ################### Tennis Stats App ###################
 ########################################################
 
-# Last update: 25-Jun-22
+# Last update: 06-Jul-22
 
-import datetime as dt, pandas as pd, streamlit as st, matplotlib.pyplot as plt
+import datetime as dt, pandas as pd, streamlit as st
+# import matplotlib.pyplot as plt
 from utils.format import auto_wide_mode
 from utils.preprocessor import app_data, player_list, filter_data, ranking_points
 from utils.plotting import scatter_chart
-from utils.preprocessor import data_processor, create_merged_data
-from utils.images import get_header
+# from utils.preprocessor import data_processor, create_merged_data
+# from utils.images import get_header
 
 # Run in wide mode
 auto_wide_mode()
 
-def pick_app():
-    st.sidebar.header('App Selection')
-    app = st.sidebar.selectbox(label='Selected App', options=['ATP Players', 'GS Forecasts'])
-    st.sidebar.markdown('---')
-    return app
+# def pick_app():
+#     st.sidebar.header('App Selection')
+#     app = st.sidebar.selectbox(label='Selected App', options=['ATP Players', 'GS Forecasts'])
+#     st.sidebar.markdown('---')
+#     return app
 
 def ATP_Tour():    
     ### Data ###
@@ -186,107 +187,108 @@ def ATP_Tour():
 ###########
 
     
-def GrandSlam():
+# def GrandSlam():
 
-    # Dataset
-    @st.cache(show_spinner=False)
-    def update_data():
-        return data_processor()
+#     # Dataset
+#     @st.cache(show_spinner=False)
+#     def update_data():
+#         return data_processor()
 
-    data = update_data()
+#     data = update_data()
 
-    st.sidebar.header('🎮 App Filters 🎮')
+#     st.sidebar.header('🎮 App Filters 🎮')
 
-    # players
-    players = pd.concat([data['Player 1'], data['Player 2'] ], axis = 0).sort_values().unique()
-    selected_player = st.sidebar.multiselect(label='Select Players', options=players)
+#     # players
+#     players = pd.concat([data['Player 1'], data['Player 2'] ], axis = 0).sort_values().unique()
+#     selected_player = st.sidebar.multiselect(label='Select Players', options=players)
 
-    # slams
-    slams = data['Grand Slam'].unique()
-    selected_slam = st.sidebar.multiselect(label='Select Slams', options=slams)
+#     # slams
+#     slams = data['Grand Slam'].unique()
+#     selected_slam = st.sidebar.multiselect(label='Select Slams', options=slams)
 
-    # years
-    years = data['Year'].unique()
-    selected_year = st.sidebar.multiselect(label='Select Years', options=years)
+#     # years
+#     years = data['Year'].unique()
+#     selected_year = st.sidebar.multiselect(label='Select Years', options=years)
 
-    # round
-    rounds = data['Round'].unique()
-    selected_round = st.sidebar.multiselect(label='Select Round', options=rounds)
+#     # round
+#     rounds = data['Round'].unique()
+#     selected_round = st.sidebar.multiselect(label='Select Round', options=rounds)
 
-    #######################
-    #### Apply Filters ####
-    #######################
+#     #######################
+#     #### Apply Filters ####
+#     #######################
 
-    # players
-    if len(selected_player) != 0:
-        data = data[(data['Player 1'].isin(selected_player)) | (data['Player 2'].isin(selected_player))]
+#     # players
+#     if len(selected_player) != 0:
+#         data = data[(data['Player 1'].isin(selected_player)) | (data['Player 2'].isin(selected_player))]
 
-    # slams
-    if len(selected_slam) > 0:
-        data = data[data['Grand Slam'].isin(selected_slam)]
+#     # slams
+#     if len(selected_slam) > 0:
+#         data = data[data['Grand Slam'].isin(selected_slam)]
 
-    # years
-    if len(selected_year) > 0:
-        data = data[data['Year'].isin(selected_year)]
-    else:
-        data = data.drop(columns=['Year'])
+#     # years
+#     if len(selected_year) > 0:
+#         data = data[data['Year'].isin(selected_year)]
+#     else:
+#         data = data.drop(columns=['Year'])
 
-    # rounds
-    if len(selected_round) > 0:
-        data = data[data['Round'].isin(selected_round)]
-
-
-    ############################################
-    ################ APP Build #################
-    ############################################
-    st.header('Grand Slam Predictions')
-
-    st.markdown ('''
-        This app is used for sharing the output from my SlamModel project I run throughout the year.
-        ''')
-
-    # Header Image & Source
-    st.image(get_header())
-    st.markdown('Image: [Josh Calabrese](https://images.unsplash.com/photo-1482614312710-79c1d29bda2a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80)')
+#     # rounds
+#     if len(selected_round) > 0:
+#         data = data[data['Round'].isin(selected_round)]
 
 
-    # Model Accuracy Chart
-    st.subheader('Model Performance...')
-    accuracy_round = data.groupby(by=['Round']).mean()['Predicted']*100
-    accuracy_overall = round(accuracy_round.sum() / 7, 1)
+#     ############################################
+#     ################ APP Build #################
+#     ############################################
+#     st.header('Grand Slam Predictions')
 
-    st.markdown(f'### Mean model accuracy: {accuracy_overall} %')
-    st.bar_chart(accuracy_round)
+#     st.markdown ('''
+#         This app is used for sharing the output from my SlamModel project I run throughout the year.
+#         ''')
 
-    # Player Results
-    st.subheader('Player Results...')
-    data_merge = create_merged_data(dataset=data)
-    df_players = pd.DataFrame(index = data_merge.mean().index)
+#     # Header Image & Source
+#     st.image(get_header())
+#     st.markdown('Image: [Josh Calabrese](https://images.unsplash.com/photo-1482614312710-79c1d29bda2a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80)')
 
-    df_players['Matches Played'] = df_players.index.map(data_merge.count()['Predicted'].to_dict())
-    df_players['Predicted'] = df_players.index.map(data_merge.sum()['Predicted'].to_dict()).astype(int)
 
-    df_players['% Accuracy'] = 100 * df_players['Predicted'] / df_players['Matches Played']
-    df_players['% Accuracy'] = [int(i) for i in df_players['% Accuracy']]
+#     # Model Accuracy Chart
+#     st.subheader('Model Performance...')
+#     accuracy_round = data.groupby(by=['Round']).mean()['Predicted']*100
+#     accuracy_overall = round(accuracy_round.sum() / 7, 1)
 
-    df_players['Mean Model Win %'] = df_players.index.map(data_merge.mean()['P1 Win %'].to_dict()).astype(int)
-    df_players['Mean Model Lose %'] = df_players.index.map(data_merge.mean()['P2 Win %'].to_dict()).astype(int)
+#     st.markdown(f'### Mean model accuracy: {accuracy_overall} %')
+#     st.bar_chart(accuracy_round)
 
-    df_players['Last Round Played'] = df_players.index.map(data_merge.max()['Round'].to_dict())
+#     # Player Results
+#     st.subheader('Player Results...')
+#     data_merge = create_merged_data(dataset=data)
+#     df_players = pd.DataFrame(index = data_merge.mean().index)
 
-    # filter 
-    if len(selected_player) > 0:
-        df_players = df_players[df_players.index.isin(selected_player)]
+#     df_players['Matches Played'] = df_players.index.map(data_merge.count()['Predicted'].to_dict())
+#     df_players['Predicted'] = df_players.index.map(data_merge.sum()['Predicted'].to_dict()).astype(int)
 
-    st.dataframe(df_players.sort_values(by = ['Predicted', '% Accuracy'], ascending=False))
+#     df_players['% Accuracy'] = 100 * df_players['Predicted'] / df_players['Matches Played']
+#     df_players['% Accuracy'] = [int(i) for i in df_players['% Accuracy']]
 
-    # Filtered Data
-    st.subheader('Filtered Results...')
-    st.dataframe(data)
+#     df_players['Mean Model Win %'] = df_players.index.map(data_merge.mean()['P1 Win %'].to_dict()).astype(int)
+#     df_players['Mean Model Lose %'] = df_players.index.map(data_merge.mean()['P2 Win %'].to_dict()).astype(int)
+
+#     df_players['Last Round Played'] = df_players.index.map(data_merge.max()['Round'].to_dict())
+
+#     # filter 
+#     if len(selected_player) > 0:
+#         df_players = df_players[df_players.index.isin(selected_player)]
+
+#     st.dataframe(df_players.sort_values(by = ['Predicted', '% Accuracy'], ascending=False))
+
+#     # Filtered Data
+#     st.subheader('Filtered Results...')
+#     st.dataframe(data)
 
 if __name__ == '__main__':
-    app = pick_app()
-    if app == 'ATP Players':
-        ATP_Tour()
-    elif app == 'GS Forecasts':
-        GrandSlam()
+    ATP_Tour()
+    # app = pick_app()
+    # if app == 'ATP Players':
+    #     ATP_Tour()
+    # elif app == 'GS Forecasts':
+    #     GrandSlam()
